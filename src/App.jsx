e579f7bcd2a2ba8d0853aaca32815b3ff7b3c87d
ghispell <ghis.pell@hotmail.fr>
2023-10-17 15:29:40 +0200

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './style.scss';
+import StarRating from "./StarRating";
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='container'>
+       <div className='description'>
+        <img src="yame.jpg" alt="Album Yame" />
+        
+          <div className='nameAlbum'>
+            <h2>Yamê</h2>
+            <h3>ELOWI (2023)</h3>
+          </div>
+          <p>Yamê est un jeune artiste franco-camerounais né d'un père musicien et d'une mère informaticienne, deux passions dont il a hérité et qui rythment sa vie. 100% autodidacte, il lance sa carrière solo en mars 2020 après avoir débuté en tant que claviériste et chanteur dans les Jams Parisiennes.</p>
+        </div>
+        <div className='rating'>
+          <p>Notez cet album :</p>
+          <div className='starRating'><StarRating /></div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
